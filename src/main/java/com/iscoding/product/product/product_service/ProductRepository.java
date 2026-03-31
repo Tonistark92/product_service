80194c93e63f0582e33a9724ad7b3d5ac535b77b
Sakai <hanyislam270@gmail.com>
2026-03-31 05:49:39 +0200

    Revert "adding the product entity and repo"
    This reverts commit 455887feae22b9212090d8b397557a93f72fd384.

--- src/main/java/com/iscoding/product/product/product_service/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.iscoding.product.product.product_service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-    Optional<ProductEntity> findByCode(String code);
-}
